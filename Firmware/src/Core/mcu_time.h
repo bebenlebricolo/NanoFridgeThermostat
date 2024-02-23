5ea25d03b5942f3d5d5b4be55e723aad1aa307ec
bebenlebricolo <benoittarrade@hotmail.fr>
2024-02-23 10:50:36 +0100

    Tweaking clang-formatc tools
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/src/Core/mcu_time.h
+++ Firmware/src/Core/mcu_time.h
@@ -2,26 +2,25 @@
 #define TIME_HEADER
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include <stdint.h>
 
 /**
  * @brief helps keeping track of time
-*/
+ */
 typedef struct
 {
-    uint32_t seconds;       /**> Counts seconds         */
-    uint16_t milliseconds;  /**> Counts milliseconds    */
+    uint32_t seconds;      /**> Counts seconds         */
+    uint16_t milliseconds; /**> Counts milliseconds    */
 } mcu_time_t;
 
 /**
  * @brief resets time construct to default. (All zeros)
  * @param[in/out] time : time construct to be reset
-*/
-void time_default(mcu_time_t * time);
+ */
+void time_default(mcu_time_t *time);
 
 #ifdef __cplusplus
 }
