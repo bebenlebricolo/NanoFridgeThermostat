9b51040c26c23d5f9db90414591d8f0d3a01722e
bebenlebricolo <benoittarrade@hotmail.fr>
2024-02-23 17:50:12 +0100

    Added uint8_t interpolation
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/src/Core/interpolation.c
+++ Firmware/src/Core/interpolation.c
@@ -1,34 +1,35 @@
-#include <stdbool.h>
 #include "interpolation.h"
+#include <stdbool.h>
 
 // Those aliasing factors are used to oversize a little
 // bit temporary results in interpolation on integer, in order to reduce
 // overall aliasing (the bigger the number the lesser the aliasing effect, so that we loose less "steps")
 #define UINT16_ALIASING_FACTOR 100
+#define UINT8_ALIASING_FACTOR 10
 
-interpolation_range_check_t interpolation_check_value_range_uint16(uint16_t const * const value, range_uint16_t const * const range)
+interpolation_range_check_t interpolation_check_value_range_uint16(uint16_t const *const value, range_uint16_t const *const range)
 {
     bool positive_range = range->start < range->end;
-    if(positive_range)
+    if (positive_range)
     {
-        if(*value < range->start)
+        if (*value < range->start)
         {
             return RANGE_CHECK_LEFT;
         }
 
-        if(*value > range->end)
+        if (*value > range->end)
         {
             return RANGE_CHECK_RIGHT;
         }
     }
     else
     {
-        if(*value > range->start)
+        if (*value > range->start)
         {
             return RANGE_CHECK_LEFT;
         }
 
-        if(*value < range->end)
+        if (*value < range->end)
         {
             return RANGE_CHECK_RIGHT;
         }
@@ -37,16 +38,47 @@ interpolation_range_check_t interpolation_check_value_range_uint16(uint16_t cons
     return RANGE_CHECK_INCLUDED;
 }
 
-int8_t interpolation_linear_uint16_to_int8(uint16_t const * const value, range_uint16_t const * const in, range_int8_t const * const out )
+interpolation_range_check_t interpolation_check_value_range_uint8(const uint8_t value, range_uint8_t const *const range)
+{
+    bool positive_range = range->start < range->end;
+    if (positive_range)
+    {
+        if (value < range->start)
+        {
+            return RANGE_CHECK_LEFT;
+        }
+
+        if (value > range->end)
+        {
+            return RANGE_CHECK_RIGHT;
+        }
+    }
+    else
+    {
+        if (value > range->start)
+        {
+            return RANGE_CHECK_LEFT;
+        }
+
+        if (value < range->end)
+        {
+            return RANGE_CHECK_RIGHT;
+        }
+    }
+
+    return RANGE_CHECK_INCLUDED;
+}
+
+int8_t interpolation_linear_uint16_to_int8(uint16_t const *const value, range_uint16_t const *const in, range_int8_t const *const out)
 {
     interpolation_range_check_t checked_value = interpolation_check_value_range_uint16(value, in);
-    switch(checked_value)
+    switch (checked_value)
     {
-        case RANGE_CHECK_LEFT :
+        case RANGE_CHECK_LEFT:
             // Clamp data to the start of output range
             return out->start;
 
-        case RANGE_CHECK_RIGHT :
+        case RANGE_CHECK_RIGHT:
             // Clamp data to the end of output range
             return out->end;
 
@@ -62,17 +94,48 @@ int8_t interpolation_linear_uint16_to_int8(uint16_t const * const value, range_u
     // By nature, value is always contained within the range we need to check.
     // As a result, (*value - in->start) <= in_delta.
     // So dividing *value - in->start is always less than 1, hence loss in precision
-    int16_t in_tmp = ((int16_t) *value - (int16_t) in->start );
+    int16_t in_tmp = ((int16_t)*value - (int16_t)in->start);
     in_tmp *= UINT16_ALIASING_FACTOR;
     in_tmp /= in_delta;
 
-    int16_t tmp_result = (in_tmp * (int16_t) out_delta);
+    int16_t tmp_result = (in_tmp * (int16_t)out_delta);
     tmp_result /= UINT16_ALIASING_FACTOR;
 
     tmp_result += out->start;
 
-    int8_t result = (int8_t) (tmp_result);
+    int8_t result = (int8_t)(tmp_result);
 
     return result;
 }
 
+uint8_t interpolation_linear_uint8_to_uint8(const uint8_t value, range_uint8_t const *const in, range_uint8_t const *const out)
+{
+    interpolation_range_check_t checked_value = interpolation_check_value_range_uint8(value, in);
+    switch (checked_value)
+    {
+        case RANGE_CHECK_LEFT:
+            // Clamp data to the start of output range
+            return out->start;
+
+        case RANGE_CHECK_RIGHT:
+            // Clamp data to the end of output range
+            return out->end;
+
+        case RANGE_CHECK_INCLUDED:
+        default:
+            break;
+    }
+
+    uint8_t in_delta = in->end - in->start;
+    uint8_t out_delta = out->end - out->start;
+
+    uint8_t in_tmp = value - in->start;
+    in_tmp *= UINT8_ALIASING_FACTOR;
+    in_tmp /= in_delta;
+
+    uint8_t tmp_result = (in_tmp * out_delta);
+    tmp_result /= UINT8_ALIASING_FACTOR;
+
+    tmp_result += out->start;
+    return tmp_result;
+}
--- Firmware/src/Core/interpolation.h
+++ Firmware/src/Core/interpolation.h
@@ -21,26 +21,21 @@ typedef enum
     RANGE_CHECK_RIGHT       /**> Value is outside the range, past the end       */
 } interpolation_range_check_t;
 
-
 /**
  * @brief Numerical range with uint16_t arithmetic
  * Note that ranges can be reversed (start is bigger than end, decreasing range)
 */
-typedef struct
-{
-    uint16_t start;
-    uint16_t end;
-} range_uint16_t;
+#define MAKE_RANGE(type) \
+    typedef struct          \
+    {                       \
+        type start;     \
+        type end;       \
+    } range_##type;
 
-/**
- * @brief Numerical range with uint16_t arithmetic
- * Note that ranges can be reversed (start is bigger than end, decreasing range)
-*/
-typedef struct
-{
-    int8_t start;
-    int8_t end;
-} range_int8_t;
+MAKE_RANGE(uint8_t);
+MAKE_RANGE(int8_t);
+MAKE_RANGE(uint16_t);
+MAKE_RANGE(int16_t);
 
 /**
  * @brief interpolates the value within the input range into the output range.
@@ -50,8 +45,20 @@ typedef struct
  * @return int8_t : input value mapped in the output range
 */
 int8_t interpolation_linear_uint16_to_int8(uint16_t const * const value,
-                                         range_uint16_t const * const input_range,
-                                         range_int8_t const * const out_range );
+                                           range_uint16_t const * const input_range,
+                                           range_int8_t const * const out_range );
+
+
+/**
+ * @brief interpolates the value within the input range into the output range.
+ * @param[in] value         : the value that needs to be converted in the output range
+ * @param[in] input_range   : input value range
+ * @param[in] output_range  : output value range
+ * @return int8_t : input value mapped in the output range
+*/
+uint8_t interpolation_linear_uint8_to_uint8(const uint8_t value,
+                                            range_uint8_t const * const input_range,
+                                            range_uint8_t const * const out_range );
 
 /**
  * @brief Checks whether an input value is contained within the input range (value pair) or not.
@@ -63,6 +70,17 @@ int8_t interpolation_linear_uint16_to_int8(uint16_t const * const value,
  *      RANGE_CHECK_RIGHT    : input value is NOT included within the two values range, and is on the right side of the range.
 */
 interpolation_range_check_t interpolation_check_value_range_uint16(uint16_t const * const value, range_uint16_t const * const range);
+
+/**
+ * @brief Checks whether an input value is contained within the input range (value pair) or not.
+ * This function uses the included version of the check : [val1, val2] instead of the excluding check ]val1,val2[.
+ * In case the input value is outside of the given range, returned enum indicates on which side the value is located.
+ * @return
+ *      RANGE_CHECK_INCLUDED : input value is included within the two values range.
+ *      RANGE_CHECK_LEFT     : input value is NOT included within the two values range, and is on the left side of the range.
+ *      RANGE_CHECK_RIGHT    : input value is NOT included within the two values range, and is on the right side of the range.
+*/
+interpolation_range_check_t interpolation_check_value_range_uint8(const uint8_t value, range_uint8_t const * const range);
 
 
 #ifdef __cplusplus
